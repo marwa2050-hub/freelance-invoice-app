506c544c805a711ac60b1ac240d468def3893597
marwa2050-hub <marwanurestani@gmail.com>
2025-09-23 17:45:25 +0430

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,3 @@
-// utils.js
 export function generateID() {
     return Date.now();
-}
-
-export function findClientById(id) {
-    return clients.find(client => client.id === id);
-}
-
-export function formatCurrency(amount) {
-    return "$" + parseFloat(amount).toFixed(2);
 }
