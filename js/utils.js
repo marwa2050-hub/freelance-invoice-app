134c98d6fcca362779516f420db13a2205666044
marwa2050-hub <marwanurestani@gmail.com>
2025-09-23 17:49:11 +0430

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,13 @@
-export function generateID() {
-    return Date.now();
+// js/utils.js
+export function generateId() {
+  return Date.now().toString() + Math.floor(Math.random()*1000).toString();
+}
+
+export function formatCurrency(num) {
+  const n = Number(num) || 0;
+  return n.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
+}
+
+export function findById(arr, id) {
+  return arr.find(x => x.id === id);
 }
